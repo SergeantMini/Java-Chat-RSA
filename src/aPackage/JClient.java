f8bb4813ed8ef3270d76f4eba91b8fea92bf3936
Monés Vela Jahnke <monica.velaje@udlap.mx>
2018-05-04 12:50:50 -0500

    Update JClient.java

--- src/aPackage/JClient.java
+++ src/aPackage/JClient.java
@@ -1,4 +1,4 @@
-package cs251lab10package;
+package aPackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,37 +14,36 @@ public class JClient {
     JChatComm chat;
     public JClient() throws ClassNotFoundException, IOException {
     	BufferedReader ans = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter IP address of server : ");
+        System.out.print("Ingrese la dirección del servidor: ");
         this.callServer(ans.readLine(), 5123);
     }
 
     public void callServer(String hostName,int portNumber) throws ClassNotFoundException, UnknownHostException, IOException {
-     	System.out.println("Trying to connect to a server ... ");
+     	System.out.println("Intentando conectar al servidor ... ");
         Socket echoSocket = new Socket(hostName, portNumber);
-        System.out.println("Connected to server : "+hostName);
+        System.out.println("Conectado al servidor "+hostName);
         ObjectOutputStream out =  new ObjectOutputStream(echoSocket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream());
-        System.out.println("Streams established. Ready for a chat ...\n");
+        System.out.println("Chat listo ...\n");
         WaitingThread wait = new WaitingThread();
-        chat = new JChatComm(echoSocket, in, out,"Client"); 
-        JPacket pkt = new JPacket(new JMessage("Hey! Want to chat? My public keys are: " + chat.myPublicKey+" and "+chat.myModulus));
-        System.out.println("Me : "+pkt.core.text);
-        System.out.println("<sent at: "+pkt.timestamp+">\n");
+        chat = new JChatComm(echoSocket, in, out,"Cliente"); 
+        JPacket pkt = new JPacket(new JMessage("Mis llaves públicas son: " + chat.myPublicKey+" and "+chat.myModulus));
+        System.out.println("Yo : "+pkt.core.text);
+        System.out.println("<enviado: "+pkt.timestamp+">\n");
         out.writeObject(pkt);
         wait.start();
         JPacket reply = (JPacket)(in.readObject());
         if ((reply.core.text).contains("Sure")) {
 	        System.out.println(hostName+ " : " + reply.core.text);
-	        System.out.println("<This maessage was sent at: "+pkt.timestamp+">");
-	        System.out.println("[This message was received at:"+ new Timestamp((new Date()).getTime())+"]");
-	        Sender sender = new Sender(chat, "Me : ");
+	        System.out.println("[Recibido:"+ new Timestamp((new Date()).getTime())+"]");
+	        Sender sender = new Sender(chat, "Yo: ");
 	        Receiver receiver = new Receiver(chat, hostName+" : ", hostName);
 	        sender.start();
 	        receiver.start();
         }
         else {
             echoSocket.close();
-            System.out.println("Time Out!");
+            System.out.println("Fin de sesión");
         }
  
     }
